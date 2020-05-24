e2da7bb757be929ef733ce10c84f0763dd366eb0
Oleg <tysenkov.oleg@gmail.com>
2020-05-24 16:09:55 +0300

    addClassPerson

--- contactForm/script.js
+++ contactForm/script.js
@@ -1,6 +1,10 @@
 let btn = document.querySelector('button[type="submit"]'),
-    numbForm = document.querySelectorAll('input[id="number"]'),
-    form = document.querySelector('form');
+    numbForm = document.querySelector('input[id="number"]'),
+    form = document.querySelector('form'),
+    fullName = form.querySelector('input[id="name"]'),
+    comment = form.querySelector('textarea[id="subject"]'),
+    send = form.querySelector('input[id="send"]');
+
 
 
 window.addEventListener('DOMContentLoaded', function () {
@@ -15,4 +19,31 @@ window.addEventListener('DOMContentLoaded', function () {
         input.placeholder = 'Ваш номер телефона..';
         insertAfter(input, btn);
     });
+
+    class Person {
+        constructor(fullName, number, comment) {
+            this.fullName = fullName;
+            this.number = number;
+            this.comment = comment;
+        }
+        send() {
+
+        }
+    }
+
+
+    const defaultPerson = new Person('Oleg', 3753333361812, 'hello');
+
+    function sendForm() {
+        fullName.value = defaultPerson.fullName;
+        numbForm.value = '+' + defaultPerson.number;
+        comment.value = defaultPerson.comment;
+        defaultPerson.send();
+    }
+
+    send.addEventListener('click', () => {
+        sendForm();
+    });
+
+
 });
